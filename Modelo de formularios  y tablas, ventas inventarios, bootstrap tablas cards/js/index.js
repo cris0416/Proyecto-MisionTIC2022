5a2ab8751a8fb6d7fc2f2b36239c40f029415d82
christianr_ruiz@yahoo.com <christianr_ruiz@yahoo.com>
2021-10-02 18:39:16 -0500

    Listas dinamicas para llenar formulario y tablas, guardar datos en objeto y este guardarlo en un arreglo, mostrar datos en celdas y filas

--- Modelo de formularios  y tablas, ventas inventarios, bootstrap tablas cards/js/index.js
+++ Modelo de formularios  y tablas, ventas inventarios, bootstrap tablas cards/js/index.js
@@ -1,3 +1,36 @@
+const productos = [];
+const clases = ['Seleccione de la lista', 'Frutas', 'Verduras', 'Abarrotes', 'Otros']
+const selectClases = document.querySelector("#clase-select");
+for(let i=0; i < clases.length; ++i) {
+    let option = document.createElement("option");
+    option.value = i;
+    option.innerText = clases[i];
+    selectClases.appendChild(option);
+}
+const cargarTabla = ()=>{
+    const tbody = document.querySelector("#tabla-tbody");
+    tbody.innerHTML = "";
+    for(let i=0; i < productos.length; ++i) {
+        let p = productos[i];
+        let fila = document.createElement("tr");
+        let celdaNombre = document.createElement("td");
+        celdaNombre.innerText = p.nombre;
+        let celdaUrl = document.createElement("td");
+        celdaUrl.innerText = p.url;
+        let celdaCodigo = document.createElement("td");
+        celdaCodigo.innerText = p.codigo;
+        let celdaClase = document.createElement("td");
+        celdaClase.innerText = p.clase;
+        let celdaDescripcion = document.createElement("td");
+        celdaDescripcion.innerText = p.descripcion;
+        fila.appendChild(celdaNombre);
+        fila.appendChild(celdaCodigo);
+        fila.appendChild(celdaClase);
+        fila.appendChild(celdaDescripcion);
+        fila.appendChild(celdaUrl);
+        tbody.appendChild(fila);
+    }
+}
 //AGREGAR UN LISTENER PARA EL EVENTO CLICK
 document.querySelector("#registrar-btn").addEventListener("click", ()=>{
     let nombre = document.querySelector("#nombre-txt").value;
@@ -5,5 +38,13 @@ document.querySelector("#registrar-btn").addEventListener("click", ()=>{
     let codigo = document.querySelector("#codigo-txt").value;
     let clase = document.querySelector("#clase-select").value;
     let descripcion = document.querySelector("#descripcion-txt").value;
-    console.log(nombre, ",", codigo, ",",clase,",", descripcion, ",", url);
+    
+    let producto = {};
+    producto.nombre = nombre;
+    producto.url = url;
+    producto.codigo = codigo;
+    producto.clase = clase;
+    producto.descripcion = descripcion;
+    productos.push(producto);
+    cargarTabla();
 });
