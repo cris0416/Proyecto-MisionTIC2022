eb9e9c9eb128ea5d07db589e146e60ac2adf4cce
christianr_ruiz@yahoo.com <christianr_ruiz@yahoo.com>
2021-10-07 21:02:46 -0500

    Empezar trabajo react

---
+++ Sprint3/formulario react/formulario/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <main>
+      <form action="">
+        <label htmlFor="">Producto</label>
+        <input type="text" placeholder="producto" />
+        <p>lorem ipsum</p>
+      </form>
+    </main>
+  );
+}
+
+export default App;
